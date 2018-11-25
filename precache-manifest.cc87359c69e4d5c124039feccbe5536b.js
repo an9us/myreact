5a5f55f1632c5ddbe11d6355756b2771b87ab16e
an9us <38591140+an9us@users.noreply.github.com>
2018-11-25 23:49:19 +0800

    Updates

---
+++ precache-manifest.cc87359c69e4d5c124039feccbe5536b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1504aa76d4768f7e96e7",
+    "url": "/myreact/static/css/main.78629ccb.chunk.css"
+  },
+  {
+    "revision": "1504aa76d4768f7e96e7",
+    "url": "/myreact/static/js/main.1504aa76.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/myreact/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "f726a0ea515ef25c0f39",
+    "url": "/myreact/static/js/runtime~main.f726a0ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "24a1cf145673213006cc0f2315ded267",
+    "url": "/myreact/index.html"
+  }
+];
